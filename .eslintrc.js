d639cec5180fb96cd462ae4081fd6381e0dc61b3
Gustavo Cabral <andrescabral.c@gmail.com>
2019-03-20 22:20:49 -0300

    chore: add new eslint rules and date packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,14 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
+  plugins: ['@getify/proper-arrows'],
   rules: {
+    'func-names': ['error', 'as-needed'],
+    '@getify/proper-arrows/params': ['error', { unused: 'trailing' }],
+    '@getify/proper-arrows/name': ['error', { trivial: false }],
+    '@getify/proper-arrows/location': ['error', { global: true }],
+    '@getify/proper-arrows/return': ['error', { object: true }],
+    '@getify/proper-arrows/this': ['error', 'always', { 'no-global': true }],
     'space-before-function-paren': 'off'
   }
 }
